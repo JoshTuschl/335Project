4ec85d2730d251fbfafb83bf6b148d3fa88c05c1
Stealth80 <Stealth80_@hotmail.com>
2013-12-14 13:15:01 -0500

    sensor listener

--- VirtualBasketballShooter/src/com/example/virtualbasketballshooter/MyGLSurfaceView.java
+++ VirtualBasketballShooter/src/com/example/virtualbasketballshooter/MyGLSurfaceView.java
@@ -46,6 +46,32 @@ public class MyGLSurfaceView extends GLSurfaceView {
         }
     }
 	
+	public class SensorActivity extends Activity, implements SensorEventListener {
+	     private final SensorManager mSensorManager;
+	     private final Sensor mAccelerometer;
+
+	     public SensorActivity() {
+	         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+	         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+	     }
+
+	     protected void onResume() {
+	         super.onResume();
+	         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+	     }
+
+	     protected void onPause() {
+	         super.onPause();
+	         mSensorManager.unregisterListener(this);
+	     }
+
+	     public void onAccuracyChanged(Sensor sensor, int accuracy) {
+	     }
+
+	     public void onSensorChanged(SensorEvent event) {
+	     }
+	 }
+	
 	@Override
 	public boolean onTouchEvent(MotionEvent e) {
 		mScaleDetector.onTouchEvent(e);
