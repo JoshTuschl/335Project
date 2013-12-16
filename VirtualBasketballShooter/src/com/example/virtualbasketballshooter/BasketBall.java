40e095c5f8af66085cae4e60f95940eb6ec201d0
Stealth80 <Stealth80_@hotmail.com>
2013-12-16 17:41:27 -0500

    basketball

--- VirtualBasketballShooter/src/com/example/virtualbasketballshooter/BasketBall.java
+++ VirtualBasketballShooter/src/com/example/virtualbasketballshooter/BasketBall.java
@@ -8,6 +8,33 @@ public class BasketBall {
 	private float backboard_bottom = 4.7f;
 	private float backboard_side = 0.75f;
 	private float FLOOR = -0.5f;
+	public float Vx;
+	public float Vy;
+	public float Vz;
+	public float x;
+	public float y;
+	public float z;
+	
+	public void updateBall(float velocity)
+	{
+		x = x + x*Vx;
+		y = y + y*Vy;
+		z = z + z*Vz;
+		
+	//	if(basketball.check_rim_collision(x, y, z))
+	//	{
+	//		//basketball.handle_rim_collision();
+	//	}
+	//	if(basketball.check_backboard_collision(x, y, z))
+	//	{
+	//		//basketball.handle_backboard_collision();
+	//	}
+	//	if(basketball.check_floor_collision(y))
+	//	{
+	//		//basketball.handle_floor_collision();
+	//	}
+	}
+	
 	
 	//rim collision detection
 	public boolean check_rim_collision(float x, float y, float z)
@@ -29,7 +56,7 @@ public class BasketBall {
 			}
 		}
 		return false;
-		
+	
 	//         velocityX = -1*(velocityX - (float)ka);
 	//         x = Xmax - RADIUS;
 	//    } else if (x - RADIUS < Xmin) {
@@ -44,8 +71,8 @@ public class BasketBall {
 	//         y = Ymin + RADIUS;
 	//    }
 		}
-			
-			
+	
+	
 		public boolean check_floor_collision(float y)
 		{
 			if(y < FLOOR + 0.01)
